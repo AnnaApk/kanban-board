03d4c1603d7d63d238e114ae24ad78811d8bf177
Anna Apkhanova <anna.karaseva.91@inbox.ru>
2023-12-05 11:53:10 +0100

    initial sample

---
+++ src/components/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import '../blocks/app/app.css';
+import Header from './Header';
+
+function App() {
+  return (
+    <div className="app">
+
+      <Header />
+      
+    </div>
+  );
+}
+
+export default App;
